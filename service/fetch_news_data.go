1aec428e280b17ff5bff14c3d3898f8460891ebf
hang.gao <hang.gao@dena.com>
2020-04-20 17:55:13 +0900

    filter illegal news

--- service/fetch_news_data.go
+++ service/fetch_news_data.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	cache "github.com/patrickmn/go-cache"
@@ -70,15 +72,19 @@ func FetchNewsData() {
 	for _, article := range news.Articles {
 		var news models.News
 
-		news.Title = article.Title + " " + article.Source.Name
-		news.Description = article.Description
-		news.UpdatedTime = models.TransferToJSTTime(article.PublishedAt)
-		news.Link = article.Url
-		news.PassedHour = int(now.Sub(news.UpdatedTime).Hours())
-		news.PassedDay = int((now.Sub(news.UpdatedTime).Hours()) / 24.0)
-		news.PassedMinutes = int(time.Now().Sub(news.UpdatedTime).Minutes())
+		if strings.Contains(article.Title, "中国") || strings.Contains(article.Title, "習近平") {
+			fmt.Println("illegal news")
+		} else {
+			news.Title = article.Title + " " + article.Source.Name
+			news.Description = article.Description
+			news.UpdatedTime = models.TransferToJSTTime(article.PublishedAt)
+			news.Link = article.Url
+			news.PassedHour = int(now.Sub(news.UpdatedTime).Hours())
+			news.PassedDay = int((now.Sub(news.UpdatedTime).Hours()) / 24.0)
+			news.PassedMinutes = int(time.Now().Sub(news.UpdatedTime).Minutes())
 
-		newsList = append(newsList, news)
+			newsList = append(newsList, news)
+		}
 	}
 
 	Cache.Set("news", newsList, cache.DefaultExpiration)
